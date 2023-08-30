0542d40fa56d804721689979d2bcc65df1d71d57
joao <joao@git.quimicosfacil.net>
2023-08-30 19:28:04 -0300

    processes

--- modules/meters/processes.go
+++ modules/meters/processes.go
@@ -1,6 +1,7 @@
 package meters
 
 import (
+	"errors"
 	"time"
 
 	"biqx.com.br/acgm_agent/modules/config"
@@ -28,27 +29,51 @@ func (m *Meter) Start(conf *config.Config) error {
 	if err != nil {
 		return err
 	}
+
 	m.Interval = *interval
 	m.stop_requested = false
+
 	go m.loop()
+
 	return nil
 }
 
 func (m *Meter) Stop() error {
 
 	m.stop_requested = true
+
+	for {
+		attempts := 0
+
+		if !m.running {
+			break
+		}
+
+		time.Sleep(500 * time.Millisecond)
+		attempts++
+		if attempts == 4 {
+			return errors.New("ERROR: Process meters timeout exiting")
+		}
+	}
+
 	return nil
 }
 
 func (m *Meter) loop() {
 
 	m.running = true
+
 	for {
 		if m.stop_requested {
 			break
 		}
-
+		// List all processes
+		// Loop through processes
+		// Filter process by path (as in config)
+		// Get several process metrics
+		// Store process metrics in the database
 	}
+
 	m.stop_requested = false
 	m.running = false
 }
